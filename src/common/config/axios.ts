619fbc770da227dc3af4a4c7e06990dc865bd56a
leeuihyun <meta212121@naver.com>
2025-09-10 15:07:04 +0900

    feat : axios refreshToken reissue 자동화

--- src/common/config/axios.ts
+++ src/common/config/axios.ts
@@ -7,4 +7,55 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        localStorage.removeItem("accessToken");
+
+        const response = await api.post(
+          "/member/reissue",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+
+        const accessToken = response.headers["authorization"];
+
+        if (accessToken) {
+          localStorage.setItem("accessToken", accessToken);
+        }
+
+        const config = error.config;
+        if (!config) {
+          return Promise.reject(error);
+        }
+
+        return api.request(config);
+      } catch (err) {
+        console.log(err);
+        window.location.href = "/login";
+      }
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
 export default api;
